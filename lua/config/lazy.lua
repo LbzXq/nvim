0a56a2e56d13bc72c450a97a3455f511a381703b
lbz <lbzeng96@gmail.com>
2024-01-17 21:48:34 +0800

    first commit

---
+++ lua/config/lazy.lua
@@ -0,0 +1,51 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- 检查并安装lazy.nvim插件（如果lazy.nvim插件不存在）
+if not vim.loop.fs_stat(lazypath) then -- 如果lazy.nvim插件不存在
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  -- 克隆lazy.nvim插件
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath) -- lazynvim插件的路径优先被搜索
+
+require("lazy").setup({
+  spec = { -- 定义需要加载的模块
+    -- add LazyVim and import its plugins
+    -- 加载LazyVim中的插件配置
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
+    -- 加载自己定义的插件配置
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
